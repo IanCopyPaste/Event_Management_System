8f291b6efad785a6181ea14ac067e4ff651dc5a6
IanCopyPaste <adote.ronadrian.molleda@gmail.com>
2026-03-07 14:03:02 +0800

    made some landing page nav animation

---
+++ frontend/js/index.js
@@ -0,0 +1,22 @@
+let lastScroll = 0;
+const upperCont = document.querySelector(".upper-container");
+const navBar = document.querySelector(".nav-container");
+
+window.addEventListener("scroll", function () {
+    let currentScroll = window.pageYOffset;
+
+    // threshold to prevent twitching
+    if (Math.abs(currentScroll - lastScroll) < 80) return;
+
+    if (currentScroll > lastScroll) {
+        // scrolling down → hide
+        upperCont.classList.add("hide");
+        navBar.style.margin = "10px 10px"
+    } else {
+        // scrolling up → show
+        upperCont.classList.remove("hide");
+        navBar.style.margin = "0px 0px 10px 0px"
+    }
+
+    lastScroll = currentScroll;
+});
