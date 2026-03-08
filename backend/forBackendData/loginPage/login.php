06c8097679ba13fe44cbcc57140bd648dba5606f
IanCopyPaste <adote.ronadrian.molleda@gmail.com>
2026-03-08 19:36:29 +0800

    calendar next to create for now

---
+++ backend/forBackendData/loginPage/login.php
@@ -0,0 +1,21 @@
+<?php
+include("../database/config.php");
+header("Content-Type: application/json");
+
+$query = "SELECT * FROM clients WHERE id = ?";
+$result = $conn->execute_query($query);
+$data = mysqli_fetch_assoc($result);
+$client = [];
+
+while($data = mysqli_fetch_assoc($result)){
+    $client[] = $data;
+}
+
+if($data){
+    echo json_encode($data);
+}else{
+    echo json_encode([
+        "status" => "not connected"
+    ]);
+}
+?>
